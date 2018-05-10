a30b4194cb4c48355915cc79c50b4d20946669f6
PanHongXi <1096728445@qq.com>
2018-05-10 16:24:30 +0800

    再次提交测试

---
+++ ajax2.php
@@ -0,0 +1,21 @@
+<?php
+header('Content-Type:text/html;charset=utf-8');
+//date_default_timezone_set('PRC');
+//echo Date('Y-m-d H:i:s');
+
+//if ($_POST['name'] == 'pan') {
+//    echo '潘琰';
+//}
+
+print_r($_GET);
+print_r($_POST);
+
+
+
+
+
+
+
+
+
+
